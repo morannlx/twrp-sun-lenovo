96958c73e78fec94087939b57c85b06e3e691661
墨染_nlx <78503566+morannlx@users.noreply.github.com>
2025-12-22 07:36:24 +0800

    Clean up initialization settings in BoardConfig.mk
    Removed unused initialization settings for vendor lib and recovery device modules.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,10 +56,6 @@ RECOVERY_BINARY_SOURCE_FILES    += $(TARGET_OUT_EXECUTABLES)/strace
 TARGET_USERIMAGES_USE_F2FS := true
 TW_USE_DMCTL               := true
 
-# Init
-TARGET_INIT_VENDOR_LIB          := //$(DEVICE_PATH):libinit_oplus_sm87xx
-TARGET_RECOVERY_DEVICE_MODULES  := libinit_oplus_sm87xx
-
 # Kernel
 BOARD_KERNEL_IMAGE_NAME     := Image
 BOARD_BOOT_HEADER_VERSION   := 4
